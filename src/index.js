e930f153d892940c001e5680ea9e7efae32ed626
Isaiah Desta <isaiahdesta@Isaiahs-MBP.attlocal.net>
2022-09-02 20:27:25 -0400

    saving for posterity.

--- src/index.js
+++ src/index.js
@@ -3,44 +3,49 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import { useEffect, useState } from 'react';
 
-function SimpleCounter() {
-  const [digitOne, setDigitOne] = useState();
-  const [digitTwo, setDigitTwo] = useState();
-  const [digitThree, setDigitThree] = useState();
-  const [digitFour, setDigitFour] = useState();
-
+const SimpleCounter = () => {
+  let [seconds, setSeconds] = useState(0);
+  let [minutes, setMinutes] = useState(0);
+  let [hours, setHours] = useState(0);
+  let [days, setDays] = useState(0);
+  
+ 
   useEffect (() => {
-    let counter = 0;
-    setInterval(function(){
-      const four = Math.floor(counter/1000);
-      const three = Math.floor(counter/100);
-      const two = Math.floor(counter/10); 
-      const one = Math.floor(counter/1);
+     const counter = setInterval(() => {
 
-      console.log(four, three, two, one);
+      setSeconds(seconds+1);
 
-      setDigitOne(one);
-      setDigitTwo(two);
-      setDigitThree(three);
-      setDigitFour(four);
+      if (seconds===59) {
+        setMinutes(minutes+1);
+        setSeconds = 0;
+        
+      }
+      if (minutes===59) {
+        setHours(hours+1);
+        setMinutes = 0;
+      }
+      if (hours===24) {
+        setDays(days+1);
+        setHours = 0;
+      }
+      console.log(seconds, minutes, hours, days);
+      ;
+    }, 1000)
 
-      counter++;
-    }, 777)
-
-  }, [])
+  }, )
   
-  return (
-    <div className="container" style={{display: "flex"}}>
-      <div className="clock">
-        <i className="fa-solid fa-clock"></i>
+    return (
+      <div className="container" style={{display: "flex"}}>
+        <div className="clock">
+          <i className="fa-solid fa-clock"></i>
+        </div>
+        
+        <div className="days">{days}</div>
+        <div className="hours">{hours}</div>
+        <div className="minutes">{minutes}</div>
+        <div className="seconds">{seconds}</div>
       </div>
-      <div className="4">{digitFour}</div>
-      <div className="3">{digitThree}</div>
-      <div className="2">{digitTwo}</div>
-      <div className="1">{digitOne}</div>
-  
-    </div>
-  );
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
